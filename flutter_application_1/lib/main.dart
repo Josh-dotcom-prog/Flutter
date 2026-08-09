9c39484c3beaa463d2a8d370b1788b736d6cd6ba
Joshua Edyangu <edyangujoshua@gmail.com>
2026-08-09 10:51:21 +0300

    flutter application

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: CounterScreen()));
+}
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('State Demo')),
+      body: Center(child: Text('$counter', style: TextStyle(fontSize: 50))),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
